bffc4d3628d50be2c0dc8a164094b455252f1a90
toastyblast <yorankerbusch@gmail.com>
2017-11-24 11:19:17 +0100

    First commit

---
+++ src/Assignment_1/APLSubAssignmentOne.java
@@ -0,0 +1,15 @@
+package Assignment_1;
+
+public class APLSubAssignmentOne {
+    //Variables...
+
+    public static void main(String[] args) {
+        new APLSubAssignmentOne().run();
+    }
+
+    public void run() {
+        //Code here...
+    }
+
+    //Other methods...
+}
---
+++ src/Assignment_1/APLSubAssignmentTwo.java
@@ -0,0 +1,15 @@
+package Assignment_1;
+
+public class APLSubAssignmentTwo {
+    //Variables...
+
+    public static void main(String[] args) {
+        new APLSubAssignmentTwo().run();
+    }
+
+    public void run() {
+        //Code here...
+    }
+
+    //Other methods...
+}
